21bd0dc7c83a3417c4e3ba30c3a54556fbdc3fd5
Megalon <125611764+MegalonVII@users.noreply.github.com>
2026-06-18 12:22:40 -0700

    chore: readd social buttons to AboutPage component

--- src/components/changeTheme/aboutPage.tsx
+++ src/components/changeTheme/aboutPage.tsx
@@ -1,6 +1,8 @@
+import { PanelSection } from '@decky/ui'
 
 import useTranslations from '../../hooks/useTranslations'
-
+import PanelSocialButton from '../settings/socialButton'
+import { SiCrowdin, SiDiscord, SiGithub, SiKofi } from 'react-icons/si'
 
 export default function AboutPage() {
   const t = useTranslations()
@@ -8,7 +10,13 @@ export default function AboutPage() {
     <div>
       <h1>{t('aboutLabel')}</h1>
       <p>{t('aboutDescription')}</p>
-
+      <h2>{t('extras')}</h2>
+      <PanelSection>
+        <PanelSocialButton icon={<SiKofi fill="#FF5E5B" />} url="https://ko-fi.com/MegalonVII">Ko-fi</PanelSocialButton>
+        <PanelSocialButton icon={<SiDiscord fill="#5865F2" />} url="https://deckbrew.xyz/discord">Discord</PanelSocialButton>
+        <PanelSocialButton icon={<SiGithub fill="#f5f5f5" />} url="https://github.com/MegalonVII/SDH-GameThemeMusic/">Github</PanelSocialButton>
+        <PanelSocialButton icon={<SiCrowdin fill="#FFFFFF" />} url="https://crowdin.com/project/sdh-gamethememusic">{t('helpTranslate')}</PanelSocialButton>
+      </PanelSection>
     </div>
   )
 }

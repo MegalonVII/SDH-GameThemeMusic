eb1af4723957251150882554a7b61e23bb699633
Megalon <megalonsba@gmail.com>
2026-07-03 12:15:48 -0700

    feat: custom docker image for backend decky CI builds

---
+++ backend/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd /plugin
+
+if ! pnpm i --frozen-lockfile; then
+  printf "error code: %s\npnpm lockfile not found or other error occurred, please report this issue to the CI maintainer and the plugin developer." "$?"
+  exit 1
+fi
+
+if ! pnpm run build; then
+  printf "error code: %s\npnpm build failed, please report this issue to the CI maintainer and the plugin developer." "$?"
+  exit 1
+fi
+
+rsync -r --exclude "src/" --exclude "__pycache__" --exclude "node_modules" /plugin/ /out/
